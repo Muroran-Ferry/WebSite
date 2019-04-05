6ae2fac3db5fe3c36dbefa7c3a26060e469d39cb
k.yamada <yetanother.yk@gmail.com>
2019-04-05 22:25:04 +0900

    bugfix

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -9,7 +9,6 @@ import AdSense from 'react-adsense'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import './mybootstrap.scss'
-import 'bootstrap'
 
 class TemplateWrapper extends React.Component {
   render() {
@@ -45,6 +44,21 @@ class TemplateWrapper extends React.Component {
           />
         </div>
         <Footer author={siteMetadata.author} />
+        <script
+          src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+          integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+          crossorigin="anonymous"
+        />
+        <script
+          src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+          integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+          crossorigin="anonymous"
+        />
+        <script
+          src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+          integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+          crossorigin="anonymous"
+        />
       </div>
     )
   }
