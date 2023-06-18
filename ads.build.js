8594311bd802d8ea7bc1559269b45977ac72ab0b
YetAnother_yk <yetanother.yk@gmail.com>
2023-06-18 15:42:54 +0900

    Add ES6 support and write adsText to file

---
+++ ads.build.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require('fs');
+
+const adsText = process.env.ADS_TEXT;
+if (adsText && adsText.length > 0) {
+  fs.writeFileSync('dist/ads.txt', adsText);
+}
