fd706d02aa60c7bcd67cffaf56dcd5a8c85dd445
yk-lab <yk-lab@users.noreply.github.com>
2023-09-23 21:06:24 +0900

    fix: hydroを修正

--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -42,55 +42,57 @@
         </div>
       </div>
     </nav>
-    <Dialog
-      as="div"
-      class="lg:hidden"
-      @close="mobileMenuOpen = false"
-      :open="mobileMenuOpen"
-    >
-      <div class="fixed inset-0 z-30"></div>
-      <DialogPanel
-        class="fixed inset-y-0 right-0 z-30 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+    <ClientOnly>
+      <Dialog
+        as="div"
+        class="lg:hidden"
+        @close="mobileMenuOpen = false"
+        :open="mobileMenuOpen"
       >
-        <div class="flex items-center justify-between">
-          <nuxt-link href="#" class="-m-1.5 p-1.5">
-            <SiteHeaderBrand />
-          </nuxt-link>
-          <button
-            type="button"
-            class="-m-2.5 rounded-md p-2.5 text-gray-700"
-            @click="mobileMenuOpen = false"
-          >
-            <span class="sr-only">メニューを閉じる</span>
-            <XMarkIcon class="h-6 w-6" aria-hidden="true" />
-          </button>
-        </div>
-        <div class="mt-6 flow-root">
-          <div class="-my-6 divide-y divide-gray-500/10">
-            <div class="space-y-2 py-6">
-              <nuxt-link
-                v-for="item in navigation"
-                :key="item.name"
-                :to="item.to"
-                class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                @click="mobileMenuOpen = false"
-                >{{ item.name }}</nuxt-link
-              >
-            </div>
-            <div class="space-y-2 py-6">
-              <nuxt-link
-                v-for="item in subNavigation"
-                :key="item.name"
-                :to="item.to"
-                class="-mx-3 block rounded-lg py-2 px-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                @click="mobileMenuOpen = false"
-                >{{ item.name }}</nuxt-link
-              >
+        <div class="fixed inset-0 z-30"></div>
+        <DialogPanel
+          class="fixed inset-y-0 right-0 z-30 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+        >
+          <div class="flex items-center justify-between">
+            <nuxt-link href="#" class="-m-1.5 p-1.5">
+              <SiteHeaderBrand />
+            </nuxt-link>
+            <button
+              type="button"
+              class="-m-2.5 rounded-md p-2.5 text-gray-700"
+              @click="mobileMenuOpen = false"
+            >
+              <span class="sr-only">メニューを閉じる</span>
+              <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div class="mt-6 flow-root">
+            <div class="-my-6 divide-y divide-gray-500/10">
+              <div class="space-y-2 py-6">
+                <nuxt-link
+                  v-for="item in navigation"
+                  :key="item.name"
+                  :to="item.to"
+                  class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  @click="mobileMenuOpen = false"
+                  >{{ item.name }}</nuxt-link
+                >
+              </div>
+              <div class="space-y-2 py-6">
+                <nuxt-link
+                  v-for="item in subNavigation"
+                  :key="item.name"
+                  :to="item.to"
+                  class="-mx-3 block rounded-lg px-3 py-2 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  @click="mobileMenuOpen = false"
+                  >{{ item.name }}</nuxt-link
+                >
+              </div>
             </div>
           </div>
-        </div>
-      </DialogPanel>
-    </Dialog>
+        </DialogPanel>
+      </Dialog>
+    </ClientOnly>
   </header>
 </template>
 
