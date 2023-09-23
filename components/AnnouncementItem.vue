c98d119196e5d0321282912e7f87ad4cccdd52f8
yk-lab <yk-lab@users.noreply.github.com>
2023-09-23 23:27:13 +0900

    feat: お知らせ表示を追加

---
+++ components/AnnouncementItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-if="announcement.linkTo">
+    <nuxt-link
+      :href="announcement.linkTo"
+      v-bind="anchorBind"
+      class="text-sm font-semibold text-sky-600 underline decoration-sky-600 hover:text-sky-800 hover:decoration-sky-800"
+    >
+      {{ announcement.title }}
+    </nuxt-link>
+  </div>
+  <div v-else class="text-sm font-semibold text-gray-900">
+    {{ announcement.title }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { Announcement } from '~/schemas/announcement_schema';
+
+interface Props {
+  announcement: Announcement;
+}
+
+const props = withDefaults(defineProps<Props>(), {});
+
+const anchorBind = computed(() => {
+  return props.announcement.linkTo &&
+    props.announcement.linkTo.startsWith('http')
+    ? {
+        target: '_blank',
+        rel: 'noopener noreferrer',
+      }
+    : {};
+});
+</script>
+
+<style></style>
---
+++ components/AnnouncementList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-if="announcements.loading">読み込み中...</div>
+  <div v-else-if="announcements.data.length === 0">お知らせはありません</div>
+  <div v-else class="space-y-1" role="list">
+    <AnnouncementItem
+      v-for="(announcement, i) in announcements.data"
+      :key="i"
+      :announcement="announcement"
+      role="listitem"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { announcementSchema } from '~/schemas/announcement_schema';
+
+import type { Announcement } from '~/schemas/announcement_schema';
+
+const announcements = ref({
+  loading: false,
+  data: [] as Announcement[],
+});
+
+try {
+  announcements.value.loading = true;
+  const { data } = await useLazyFetch(
+    'https://api.sssapi.app/Gk4QDu_ehuuZB89c6Sx68'
+  );
+  announcements.value.data = announcementSchema.array().parse(data.value);
+} catch (error) {
+  console.error(error);
+} finally {
+  announcements.value.loading = false;
+}
+</script>
+
+<style></style>
