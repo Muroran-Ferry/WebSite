c7324a701bc86ab8136dca14f8ea1ddaa20b3178
yk-lab <yk-lab@users.noreply.github.com>
2023-10-09 18:13:55 +0900

    feat: 室蘭市のホームページより駐車場情報を追加 ALL-5

---
+++ components/Organisms/AccessItem.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
+    <dt class="text-sm font-medium text-gray-700 sm:pl-8">
+      <slot name="title"></slot>
+    </dt>
+    <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+      <slot name="default"></slot>
+    </dd>
+  </div>
+</template>
